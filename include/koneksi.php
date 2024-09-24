f2a9a87f96831333e3eefabc3f3394cb1ac14dfb
KINGBOTs1 <ds767365@gmail.com>
2024-09-24 16:11:37 +0800

    Update koneksi.php

--- include/koneksi.php
+++ include/koneksi.php
@@ -1,25 +1,12 @@
 <?php
-require '../vendor/autoload.php'; // Sesuaikan jalur jika perlu
-
-// Mengatur jalur ke file .env
-$dotenvPath = __DIR__ . '/../.env';
-if (file_exists($dotenvPath)) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../'); // Jalur ke folder yang tepat
-    $dotenv->load();
-
-    // Ambil nilai dari variabel lingkungan
-    $server = getenv('DB_SERVER');
-    $username = getenv('DB_USERNAME');
-    $password = getenv('DB_PASSWORD');
-    $database = getenv('DB_DATABASE');
-} else {
-    // Jika .env tidak ada, gunakan nilai default
-    $server = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "laundry";
-}
-
+// $server = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "laundry";
+$server = "uskkokws4w48444gw48c8048";
+$username = "root";
+$password = "jJrI3rG74rjvqIVrD56XSfM2T44DxZAuktMEHMlHLSezuuaqB60FbLQ8eJgfi0DN";
+$database = "laundry";
 // Koneksi ke database
 $conn = mysqli_connect($server, $username, $password, $database);
 
@@ -27,4 +14,4 @@ $conn = mysqli_connect($server, $username, $password, $database);
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
-echo "Koneksi berhasil";
+
